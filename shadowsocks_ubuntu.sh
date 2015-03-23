0c0f9f40f221ba7b9cb7c79c6b7dff172df38aa1
HyanCat <hyancat@live.cn>
2015-03-23 18:07:17 +0800

    添加shadowsocks脚本

---
+++ shadowsocks_ubuntu.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# ubuntu
+
+apt-get install python-pip
+pip install shadowsocks
+
+
+IP=`wget -q -O - http://api.ipify.org`
+
+PASS=`openssl rand 6 -base64`
+if [ "$1" != "" ]
+then PASS=$1
+fi
+
+
+cat >/etc/shadowsocks.json <<END
+{
+    "server":"$IP",
+    "server_port":9388,
+    "local_address": "127.0.0.1",
+    "local_port":1080,
+    "password":"$PASS",
+    "timeout":300,
+    "method":"aes-256-cfb",
+    "fast_open": false
+}
+END
+
+nohup ssserver -c /etc/shadowsocks.json &
