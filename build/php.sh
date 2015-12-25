77ab1b150353c55cdc558f03fd097d2321d972eb
HyanCat <hyancat@live.cn>
2015-12-25 15:50:16 +0800

    update.

--- build/php.sh
+++ build/php.sh
@@ -19,7 +19,7 @@ cd php-$PHP_VERSION
 
 rpm -ivh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
 
-yum install -y libxml2-devel bzip2-devel libcurl-devel libpng-devel freetype-devel libc-client-devel libjpeg-devel libmcrypt-devel
+yum install -y libxml2-devel bzip2-devel libcurl-devel libpng-devel freetype-devel libc-client-devel libjpeg-devel libmcrypt-devel openssl-devel
 ln -s /usr/lib64/libc-client.so /usr/lib/
 
 ./configure --prefix=$DEST_DIR/php --with-config-file-path=$DEST_DIR/php/etc --enable-fpm --with-mcrypt --with-zlib --enable-mbstring --with-openssl --with-mysqli --with-mysql-sock --with-gd --with-jpeg-dir --enable-gd-native-ttf --enable-pdo --with-pdo-mysql --with-gettext --with-curl --with-pdo-mysql --enable-sockets --enable-bcmath --enable-xml --with-bz2 --enable-zip --with-freetype-dir --with-imap --with-imap-ssl --with-kerberos --enable-opcache
