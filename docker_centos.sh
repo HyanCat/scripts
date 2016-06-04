a07642586b18dc3d285e6ad1d4e7330a505e5e96
HyanCat <hyancat@live.cn>
2016-06-04 22:00:08 +0800

    add docker

---
+++ docker_centos.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+yum update -y
+
+
+tee /etc/yum.repos.d/docker.repo <<-'EOF'
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/centos/$releasever/
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+
+yum install docker-engine -y
+
+service docker start
+
+curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+
+ln -s /usr/local/bin/docker-compose /usr/local/bin/dc
