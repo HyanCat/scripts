c250399c4b39e72e62648fca446612b187b26a40
流光不加少 <hyancat@live.cn>
2017-04-15 10:34:27 +0800

    Update docker_centos.sh

--- docker_centos.sh
+++ docker_centos.sh
@@ -2,21 +2,32 @@
 
 yum update -y
 
+## for docker-engine
+#tee /etc/yum.repos.d/docker.repo <<-'EOF'
+#[dockerrepo]
+#name=Docker Repository
+#baseurl=https://yum.dockerproject.org/repo/main/centos/$releasever/
+#enabled=1
+#gpgcheck=1
+#gpgkey=https://yum.dockerproject.org/gpg
+#EOF
 
-tee /etc/yum.repos.d/docker.repo <<-'EOF'
-[dockerrepo]
-name=Docker Repository
-baseurl=https://yum.dockerproject.org/repo/main/centos/$releasever/
-enabled=1
-gpgcheck=1
-gpgkey=https://yum.dockerproject.org/gpg
-EOF
+#yum install docker-engine -y
 
-yum install docker-engine -y
+## for docker-ce
 
-service docker start
+yum install -y yum-utils
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+yum makecache fast
+yum install -y docker-ce
 
-curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+systemctl start docker
+
+## install docker-compose
+
+curl -L https://github.com/docker/compose/releases/download/1.12.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
 
