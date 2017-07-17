7521c1175c31b881811d1dfdde96838e524fac79
流光不加少 <hyancat@live.cn>
2017-07-17 13:36:14 +0800

    Update docker_centos.sh
    Update docker compose version.

--- docker_centos.sh
+++ docker_centos.sh
@@ -27,7 +27,7 @@ systemctl start docker
 
 ## install docker-compose
 
-curl -L https://github.com/docker/compose/releases/download/1.12.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
 
