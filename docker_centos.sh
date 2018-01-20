26a83a2fcc602ddfb57238aece72782997cf0f30
流光不加少 <hyancat@live.cn>
2018-01-20 22:18:07 +0800

    Update docker-compose version.

--- docker_centos.sh
+++ docker_centos.sh
@@ -1,18 +1,6 @@
-#!/bin/bash
+#!/bin/bash -e
 
 yum update -y
-
-## for docker-engine
-#tee /etc/yum.repos.d/docker.repo <<-'EOF'
-#[dockerrepo]
-#name=Docker Repository
-#baseurl=https://yum.dockerproject.org/repo/main/centos/$releasever/
-#enabled=1
-#gpgcheck=1
-#gpgkey=https://yum.dockerproject.org/gpg
-#EOF
-
-#yum install docker-engine -y
 
 ## for docker-ce
 
@@ -27,7 +15,9 @@ systemctl start docker
 
 ## install docker-compose
 
-curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+DC_VERSION=1.18.0
+
+curl -L https://github.com/docker/compose/releases/download/$DC_VERSION/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
 
