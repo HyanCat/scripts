f498e9d8e07926450fca83f45127748ead9229b5
HyanCat <hyancat@live.cn>
2018-06-16 12:02:08 +0800

    update phpext.

--- build/phpext.sh
+++ build/phpext.sh
@@ -2,33 +2,21 @@
 
 ###################################################
 # PHP-EXTENSIONS
-
-yum install -y autoconf
+###################################################
 
 ## memcached
 yum install -y libmemcached-devel
 
-PHP_MEMCACHED_VER=2.2.0
-PHP_MEMCACHED=http://pecl.php.net/get/memcached-$PHP_MEMCACHED_VER.tgz
-wget $PHP_MEMCACHED
-tar -zxf memcached-$PHP_MEMCACHED_VER.tgz
-cd ./memcached-$PHP_MEMCACHED_VER
-phpize
-./configure
-make
-make install
-cd -
+pecl install memcached
 
 ## imagick
 yum install -y ImageMagick-devel
 
-PHP_IMAGICK_VER=3.1.2
-PHP_IMAGICK=http://pecl.php.net/get/imagick-$PHP_IMAGICK_VER.tgz
-wget $PHP_IMAGICK
-tar -zxf imagick-$PHP_IMAGICK_VER.tgz
-cd ./imagick-$PHP_IMAGICK_VER
-phpize
-./configure
-make
-make install
-cd -
+pecl install imagick
+
+## mongodb
+pecl install mongodb
+
+
+wget http://www.imagemagick.org/download/ImageMagick.tar.gz
+tar -zxf ImageMagick.tar.gz
